1f5cea3c04f641af6dd5cd713992af698e883031
lmarchet <lucasmarchetti21@gmail.com>
2025-01-27 14:35:32 +0100

    physical run system

--- components/playerDataComponent.js
+++ components/playerDataComponent.js
@@ -6,5 +6,9 @@ export const PlayerDataComponent = {
    gravity: 10,
    jumpForce: 300,
    maxFallSpeed: 500,
-   
+   mass: 1.5,
+   // Friction
+   x_acceleration: 10000,
+   braking_force: 20000, 
+   x_max_speed: 2000,
 }
--- systems/runSystem.js
+++ systems/runSystem.js
@@ -1,18 +1,28 @@
-export default class RunSystem{
+export default class RunSystem {
    // Basic player horizontal movements
-   update(entities, dt){
-      for(const entity of entities){
+   update(entities, dt) {
+      for (const entity of entities) {
          const position = entity.getComponent('position');
          const velocity = entity.getComponent('velocity');
          const input = entity.getComponent('input');
-         const state = entity.getComponent('state');
-         const playerData = entity.getComponent('data')
-
-         if(position && velocity){
+         const playerData = entity.getComponent('data');
+         if (position && velocity) {
             input.update();
-            position.x += velocity.vx * input.x;
-           
-            //console.log(position.x)
+            const acceleration = (input.x * playerData.x_acceleration) / playerData.mass;
+            velocity.vx += acceleration * dt;
+            if (input.x === 0 || Math.sign(input.x) !== Math.sign(velocity.vx)) {
+               const brakingForce = playerData.braking_force * dt;
+               if (velocity.vx > 0) {
+                  velocity.vx -= brakingForce;
+                  if (velocity.vx < 0) velocity.vx = 0;
+               } else if (velocity.vx < 0) {
+                  velocity.vx += brakingForce;
+                  if (velocity.vx > 0) velocity.vx = 0;
+               }
+            }
+            if (velocity.vx > playerData.x_max_speed) velocity.vx = playerData.x_max_speed;
+            else if (velocity.vx < -playerData.x_max_speed) velocity.vx = -playerData.x_max_speed;
+            position.x += velocity.vx * dt;
          }
       }
    }
