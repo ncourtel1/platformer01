858ea1a3c0f12cd2ee96cf9c7db598d1dd861b4e
ncourtel1 <courtel.nattan19@gmail.com>
2025-01-28 13:28:08 +0100

    fps set

--- main.js
+++ main.js
@@ -34,6 +34,28 @@ ecs.addSystem(new GravitySystem());
 
 let lastTime = performance.now();
 
+
+function fpsMeter() {
+   let prevTime = Date.now(),
+       frames = 0;
+
+   requestAnimationFrame(function loop() {
+     const time = Date.now();
+     frames++;
+     if (time > prevTime + 1000) {
+       let fps = Math.round( ( frames * 1000 ) / ( time - prevTime ) );
+       prevTime = time;
+       frames = 0;
+
+       console.log('FPS: ', fps);
+     }
+
+     requestAnimationFrame(loop);
+   });
+ }
+ 
+ fpsMeter();
+
 function gameLoop(time){
    const dt = (time - lastTime) / 1000;
    lastTime = time;
